2c5a5cf942586599a2c80e310cfc1b9e33d1a1f4
Aichatou Boubacar <71350385+Aichato@users.noreply.github.com>
2023-02-16 16:02:34 +0100

    Add files via upload

---
+++ changeProperty.js
@@ -0,0 +1,11 @@
+const codeloccol = {
+ name: 'Coder',
+ legs: 4,
+ tails: 1,
+ friends: ['Codeloccol Campers'],
+};
+codeloccol.name = 'Happy Coder';
+function code(codeloccol){
+     console.log(codeloccol);
+ }
+code(codeloccol);
